ad53670111c6f29f895d23f7f9743f2621fcc57c
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-10-06 23:10:23 +0200

    Template rendering using Thymeleaf

--- src/nl/timvandijkhuizen/commerce/services/WebService.java
+++ src/nl/timvandijkhuizen/commerce/services/WebService.java
@@ -1,6 +1,13 @@
 package nl.timvandijkhuizen.commerce.services;
 
 import java.io.File;
+import java.util.Map;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.buffer.PooledByteBufAllocator;
@@ -22,6 +29,7 @@ public class WebService extends BaseService {
 	private static final EventLoopGroup THREAD_GROUP = new NioEventLoopGroup(4);
 	
 	private ServerBootstrap bootstrap;
+	private TemplateEngine templateEngine;
 	
     @Override
     public String getHandle() {
@@ -33,6 +41,7 @@ public class WebService extends BaseService {
     	YamlConfig config = Commerce.getInstance().getConfig();
         
         // Create SSL context
+    	// =================================================
         ConfigOption<File> optionSslCert = config.getOption("general.sslCertificate");
         ConfigOption<File> optionSslKey = config.getOption("general.sslPrivateKey");
         SslContext sslContext = null;
@@ -44,7 +53,8 @@ public class WebService extends BaseService {
     		).build();
         }
         
-        // Configure server
+        // Setup web-server
+        // =================================================
         bootstrap = new ServerBootstrap();
         
         bootstrap.option(ChannelOption.SO_BACKLOG, 1024);
@@ -67,6 +77,46 @@ public class WebService extends BaseService {
         } catch(Exception e) {
             ConsoleHelper.printError("Failed to start webserver on port " + port, e);
         }
+        
+        // Setup template engine
+        // =================================================
+        templateEngine = new TemplateEngine();
+        
+        // Add file resolver
+        FileTemplateResolver fileResolver = new FileTemplateResolver();
+        
+        fileResolver.setPrefix(Commerce.getInstance().getDataFolder().getAbsolutePath() + File.separatorChar);
+        fileResolver.setSuffix(".html");
+        fileResolver.setCharacterEncoding("UTF-8");
+        fileResolver.setTemplateMode(TemplateMode.HTML);
+        fileResolver.setOrder(1);
+        fileResolver.setCacheable(true);
+        
+        templateEngine.addTemplateResolver(fileResolver);
+        
+        // Add class loader resolver
+        ClassLoaderTemplateResolver resourceResolver = new ClassLoaderTemplateResolver();
+        
+        resourceResolver.setPrefix("templates" + File.separatorChar);
+        resourceResolver.setSuffix(".html");
+        resourceResolver.setCharacterEncoding("UTF-8");
+        resourceResolver.setTemplateMode(TemplateMode.HTML);
+        resourceResolver.setOrder(2);
+        resourceResolver.setCacheable(true);
+        
+        templateEngine.addTemplateResolver(resourceResolver);
+    }
+    
+    public String renderTemplate(String templateName, Map<String, Object> variables) {
+        Context context = new Context();
+        
+        context.setVariables(variables);
+        
+        return templateEngine.process(templateName, context);
+    }
+    
+    public String renderTemplate(File templateFile, Map<String, Object> variables) {
+        return renderTemplate(templateFile.getPath(), variables);
     }
 	
 }
