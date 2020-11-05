57a69b5e0dd0ff10f49276be62f9a8a21ab7ac07
Tim van Dijkhuizen <timvandijkhuizen@hotmail.nl>
2020-11-05 23:12:34 +0100

    Improved terms input GUI

--- src/nl/timvandijkhuizen/commerce/config/types/ConfigTypeTerms.java
+++ src/nl/timvandijkhuizen/commerce/config/types/ConfigTypeTerms.java
@@ -6,6 +6,7 @@ import java.util.function.Consumer;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 
@@ -60,9 +61,9 @@ public class ConfigTypeTerms implements ConfigType<List<String>> {
 
     @Override
     public void getValueInput(OptionConfig config, ConfigOption<List<String>> option, MenuClick event, Consumer<List<String>> callback) {
-        Menu menu = new Menu("Terms & Conditions", MenuSize.LG);
+        Menu menu = new Menu("Place written book", MenuSize.MD);
         Player player = event.getPlayer();
-        
+
         // Add background items
         MenuItemBuilder gray = MenuItems.BACKGROUND.clone();
         MenuItemBuilder green = new MenuItemBuilder(XMaterial.LIME_STAINED_GLASS_PANE);
@@ -70,65 +71,61 @@ public class ConfigTypeTerms implements ConfigType<List<String>> {
         menu.setItem(gray, 0);
         menu.setItem(gray, 1);
         menu.setItem(gray, 2);
-        menu.setItem(green, 3);
+        menu.setItem(gray, 3);
         menu.setItem(green, 4);
         menu.setItem(green, 5);
-        menu.setItem(gray, 6);
+        menu.setItem(green, 6);
         menu.setItem(gray, 7);
         menu.setItem(gray, 8);
+        
         menu.setItem(gray, 9);
         menu.setItem(gray, 10);
         menu.setItem(gray, 11);
-        menu.setItem(green, 12);
-        menu.setItem(green, 14);
-        menu.setItem(gray, 15);
+        menu.setItem(gray, 12);
+        menu.setItem(green, 13);
+        menu.setItem(green, 15);
         menu.setItem(gray, 16);
         menu.setItem(gray, 17);
+        
         menu.setItem(gray, 18);
         menu.setItem(gray, 19);
         menu.setItem(gray, 20);
-        menu.setItem(green, 21);
+        menu.setItem(gray, 21);
         menu.setItem(green, 22);
         menu.setItem(green, 23);
-        menu.setItem(gray, 24);
+        menu.setItem(green, 24);
         menu.setItem(gray, 25);
         menu.setItem(gray, 26);
-        menu.setItem(gray, 27);
-        menu.setItem(gray, 28);
-        menu.setItem(gray, 29);
-        menu.setItem(gray, 30);
-        menu.setItem(gray, 32);
-        menu.setItem(gray, 33);
-        menu.setItem(gray, 34);
-        menu.setItem(gray, 35);
         
-        // Add back item
+        // Add cancel item
         MenuItemBuilder backItem = MenuItems.BACK.clone();
 
         backItem.setClickListener(backClick -> {
+            List<String> value = getValue(config, option);
+            
             UI.playSound(player, UI.SOUND_CLICK);
-            callback.accept(getValue(config, option));
+            callback.accept(value);
         });
 
-        menu.setItem(backItem, 31);
+        menu.setItem(backItem, 11);
         
         // Add menu click listener
         menu.addClickListener(click -> {
-            if(click.getSlot() == 13) {
+            int slot = click.getSlot();
+            
+            if(slot == 14) {
+                InventoryAction action = click.getAction();
                 ItemStack item = click.getCursor();
                 Material type = item.getType();
                 
-                if(type == Material.WRITTEN_BOOK) {
+                if(action == InventoryAction.PLACE_ALL && type == Material.WRITTEN_BOOK) {
                     BookMeta meta = (BookMeta) item.getItemMeta();
-                    List<String> pages = meta.getPages();
-                    
-                    // Accept book
+
                     UI.playSound(player, UI.SOUND_CLICK);
                     click.setCancelled(false);
                     
-                    // Call callback with delay to prevent dropping the book
                     Bukkit.getScheduler().runTaskLater(Commerce.getInstance(), () -> {
-                        callback.accept(pages);
+                        callback.accept(meta.getPages());
                     }, 1);
                 } else {
                     UI.playSound(player, UI.SOUND_ERROR);
